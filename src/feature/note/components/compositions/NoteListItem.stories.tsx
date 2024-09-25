677cf366aa22bf210fbc40b4186dc163633fb66b
Keii -けい- <keita19981209onepiece@gmail.com>
2024-09-25 18:23:39 +0900

    fix

--- src/feature/note/components/compositions/NoteListItem.stories.tsx
+++ src/feature/note/components/compositions/NoteListItem.stories.tsx
@@ -24,14 +24,14 @@ type TStory = StoryObj<typeof meta>;
 export const Default: TStory = {
   args: {
     note: {
-      noteId: 1,
       title: 'ノートタイトルタイトルタイトル',
       createdAt: '2024/12/31',
-      updateAt: '2024/12/31',
+      updatedAt: '2024/12/31',
       uuid: 'dfdfd',
       projectId: 'DVLG',
       icon: '',
       body: '本文ぶん'
-    }
+    },
+    searchTerm: ''
   }
 };
